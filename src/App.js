9143f760fb3df5d782e6aa0886a2e0dcb9845517
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2020-03-02 20:33:43 +0700

    Signed-off-by: dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
    30 menit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './TodoList';
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <TodoList />
+        <input type="text"></input>
+        <button>add</button>
+        <button>clear</button>
+        <div>0 left to do</div>
+      </>
   );
 }
 
